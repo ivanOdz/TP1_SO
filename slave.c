a89521612d9a1dc9f7f2b9ebae52e01af5d6bb49
tomaScheffer <tscheffer@itba.edu.ar>
2024-03-29 21:09:50 -0300

    Mañana definimos las sintaxis!

--- slave.c
+++ slave.c
@@ -1,2 +1,18 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+int main() {
+
+    // Mañana definimos las reglas de sintaxis
+
+    int hola = 0;
+
+    if (hola) {
+
+    }
+
+    if (hola)
+    {
+
+    }
+}
