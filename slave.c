197d17ceeeee9f72191f9615362870b8c99e3e91
ivanOdz <miodzomek@gmail.com>
2024-04-01 02:51:28 +0000

    Slave Structured

--- slave.c
+++ slave.c
@@ -1,18 +1,83 @@
-// This is a personal academic project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+// Slave: Recibe los paths a los archivos que hay que procesar con el proceso "md5sum", luego enviar el hash generado al proceso app mediante pipe.
+// Deberia esperar que el padre cierre el pípe, la lectura devuelve 0 y (tamb significa End of File). getline(3): si no se libera *line vamos a tener un memory leak
 
-int main() {
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-    // Mañana definimos las reglas de sintaxis
+#define HASHING_ALGORITHM   "./md5sum"
+#define NUMBER_ARGV_HA      2
+#define HA_RESULT_BYTES     33
 
-    int hola = 0;
+#define ORDINARY_ERROR      -1
+#define FORK_CHILD          0
+#define FORK_ERROR          ORDINARY_ERROR
 
-    if (hola) {
+#define READ_END            0
+#define WRITE_END           1
 
-    }
+void ordinaryErrorHandler(int status);
 
-    if (hola)
-    {
+int main(int argc, char *argv[]) {
 
-    }
+    int pid;
+    int status;
+    int pipeFd[2];
+    char *argvForHA[NUMBER_ARGV_HA];
+    char *envpForHA = NULL;
+    char *pathOfFileForHA;
+    char algorithmResult[HA_RESULT_BYTES];
+
+    argvForHA[NUMBER_ARGV_HA-1] = NULL;
+
+    do {
+
+        // read(pathOfFileForHA, lineLen, ?);
+        if (pipe(pipeFd) == ORDINARY_ERROR) {
+
+            ordinaryErrorHandler(status);
+        }
+
+        pid = fork();
+
+        if (pid == FORK_CHILD) {
+            
+            // close(?);
+            close(pipeFd[READ_END]);
+            dup2(pipeFd[WRITE_END], STDOUT_FILENO);
+            close(pipeFd[WRITE_END]);
+
+            pathOfFileForHA = "?";
+            argvForHA[NUMBER_ARGV_HA-2] = pathOfFileForHA;
+
+            if (execve(HASHING_ALGORITHM, argvForHA, envpForHA) == 0) {
+
+                ordinaryErrorHandler(status);
+            }
+        }
+        else if (pid != FORK_ERROR) {
+
+            close(pipeFd[WRITE_END]);
+            waitpid(pid, &status, 0);
+
+            if (read(pipeFd[READ_END], algorithmResult, sizeof(algorithmResult)) != sizeof(algorithmResult)) {
+                ordinaryErrorHandler(status);
+            }
+            close(pipeFd[READ_END]);
+            //write(algorithmResult, sizeof(algorithmResult), ?);
+        }
+        else {
+
+            ordinaryErrorHandler(status);
+        }
+
+    } while (status != ORDINARY_ERROR); // && Tenga archivos por procesar
+
+    status = 1;
+    return status;
+}
+
+inline void ordinaryErrorHandler(int status) {
+
+    exit(status);
 }
