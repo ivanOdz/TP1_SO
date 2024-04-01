cb6fdce024548eab08a4fd047ffd3d629a94e1bc
tomaScheffer <tscheffer@itba.edu.ar>
2024-04-01 16:38:42 -0300

    lectura de paths

--- slave.c
+++ slave.c
@@ -2,8 +2,9 @@
 // Deberia esperar que el padre cierre el pípe, la lectura devuelve 0 y (tamb significa End of File). getline(3): si no se libera *line vamos a tener un memory leak
 
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define HASHING_ALGORITHM   "./md5sum"
 #define NUMBER_ARGV_HA      2
@@ -16,6 +17,8 @@
 #define READ_END            0
 #define WRITE_END           1
 
+#define BUFFER_SIZE         1024
+
 void ordinaryErrorHandler(int status);
 
 int main(int argc, char *argv[]) {
@@ -27,12 +30,30 @@ int main(int argc, char *argv[]) {
     char *envpForHA = NULL;
     char *pathOfFileForHA;
     char algorithmResult[HA_RESULT_BYTES];
+    char buffer[BUFFER_SIZE];
+    int bufferBytes = 0;
 
     argvForHA[NUMBER_ARGV_HA-1] = NULL;
 
     do {
 
-        // read(pathOfFileForHA, lineLen, ?);
+        if (bufferBytes <= 1) {
+
+            bufferBytes = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+
+            if (bufferBytes < 1) {
+
+                status = EXIT_SUCCESS;
+                break;
+            }
+        }
+
+        do { // Buscar próximo Path (se consumen de forma LIFO)
+            
+            bufferBytes--;
+
+        } while (bufferBytes && buffer[bufferBytes] != ' ');
+
         if (pipe(pipeFd) == ORDINARY_ERROR) {
 
             ordinaryErrorHandler(status);
@@ -42,12 +63,11 @@ int main(int argc, char *argv[]) {
 
         if (pid == FORK_CHILD) {
             
-            // close(?);
             close(pipeFd[READ_END]);
-            dup2(pipeFd[WRITE_END], STDOUT_FILENO);
+            dup2(pipeFd[WRITE_END], STDOUT_FILENO); 
             close(pipeFd[WRITE_END]);
 
-            pathOfFileForHA = "?";
+            pathOfFileForHA = buffer + bufferBytes;
             argvForHA[NUMBER_ARGV_HA-2] = pathOfFileForHA;
 
             if (execve(HASHING_ALGORITHM, argvForHA, envpForHA) == 0) {
@@ -58,22 +78,25 @@ int main(int argc, char *argv[]) {
         else if (pid != FORK_ERROR) {
 
             close(pipeFd[WRITE_END]);
-            waitpid(pid, &status, 0);
 
             if (read(pipeFd[READ_END], algorithmResult, sizeof(algorithmResult)) != sizeof(algorithmResult)) {
+
                 ordinaryErrorHandler(status);
             }
+
+            waitpid(pid, &status, 0);
+
             close(pipeFd[READ_END]);
-            //write(algorithmResult, sizeof(algorithmResult), ?);
+
+            write(algorithmResult, sizeof(algorithmResult), STDOUT_FILENO);
         }
         else {
 
             ordinaryErrorHandler(status);
         }
 
-    } while (status != ORDINARY_ERROR); // && Tenga archivos por procesar
+    } while (1);
 
-    status = 1;
     return status;
 }
 
