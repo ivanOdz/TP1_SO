bde338b0f663bde8af97b032866d999baceade92
ivanOdz <miodzomek@gmail.com>
2024-04-02 17:23:54 -0300

    slave style

--- slave.c
+++ slave.c
@@ -1,9 +1,6 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
-// Slave: Recibe los paths a los archivos que hay que procesar con el proceso "md5sum", luego enviar el hash generado al proceso app mediante pipe.
-// Deberia esperar que el padre cierre el pípe, la lectura devuelve 0 y (tamb significa End of File). getline(3): si no se libera *line vamos a tener un memory leak
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,76 +8,65 @@
 #include <string.h>
 
 #define HASHING_ALGORITHM   "md5sum"
-#define NUMBER_ARGV_HA      4
+
+#define DEFAULT_BUFFER_SIZE 1024
 #define HA_RESULT_SIZE      32
 #define HA_BUFFER_SIZE      256
 
 #define ORDINARY_ERROR      -1
-#define FORK_CHILD          0
-#define FORK_ERROR          ORDINARY_ERROR
 
 #define READ_END            0
 #define WRITE_END           1
 
-#define DEFAULT_BUFFER_SIZE 1024
-
 int main(int argc, char *argv[]) {
 
-    int status = EXIT_FAILURE;
-
-    char *pathOfFileForHA;
+    char * pathOfFileForHA;
     char algorithmResult[HA_RESULT_SIZE];
     char readBuffer[DEFAULT_BUFFER_SIZE];
     char writeBuffer[DEFAULT_BUFFER_SIZE];
+    char commandBuff[DEFAULT_BUFFER_SIZE];
+
+    int status = EXIT_FAILURE;
     int pendingReadBufferBytes = 0;
     int writeBufferBytes = 0;
     int myPid = getpid();
     int firstRead = 1;
+
     FILE * result;
 
     do {
-
         if (pendingReadBufferBytes == 0) {
-
             pendingReadBufferBytes = read(STDIN_FILENO, readBuffer, sizeof(readBuffer));
-    
-            if (pendingReadBufferBytes < 1) {
 
+            if (pendingReadBufferBytes < 1) {
                 status = EXIT_SUCCESS;
                 break;
             }
 
             if (readBuffer[pendingReadBufferBytes-1] == '\n') {
-
-                pendingReadBufferBytes--;
+                readBuffer[--pendingReadBufferBytes] = '\0';
             }
 
-            readBuffer[pendingReadBufferBytes] = '\0';
-
             if (!firstRead) {
-
                 pathOfFileForHA = readBuffer;
                 pendingReadBufferBytes = 0;
             }
         }
 
         if (firstRead) {
-
             while (pendingReadBufferBytes && readBuffer[pendingReadBufferBytes-1] != '\0') {
-
                 pendingReadBufferBytes--;
             }
+        
             if (pendingReadBufferBytes == 0) {
-                
                 firstRead = 0;
             }
 
             pathOfFileForHA = readBuffer + pendingReadBufferBytes;
         }
 
-        char command[DEFAULT_BUFFER_SIZE];
-        snprintf(command, DEFAULT_BUFFER_SIZE, "%.*s %.*s", (int)strlen(HASHING_ALGORITHM), HASHING_ALGORITHM, (int)strlen(pathOfFileForHA), pathOfFileForHA);
-        result = popen(command, "r");
+        snprintf(commandBuff, DEFAULT_BUFFER_SIZE, "%.*s %.*s", (int)strlen(HASHING_ALGORITHM), HASHING_ALGORITHM, (int)strlen(pathOfFileForHA), pathOfFileForHA);
+        result = popen(commandBuff, "r");
     
         if (result == NULL) {
             perror("popen");
@@ -92,7 +78,6 @@ int main(int argc, char *argv[]) {
         write(STDOUT_FILENO, writeBuffer, writeBufferBytes + 1);
         
     } while (1);
-
 
     return status;
 }
